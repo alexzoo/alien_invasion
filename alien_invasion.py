1e4518cdd31fe9d9dfd322c8eeb6591af9983808
azubov <zubov.alexey@gmail.com>
2022-10-08 15:30:52 +0300

    add main class AlienInvasion

---
+++ alien_invasion.py
@@ -0,0 +1,34 @@
+import sys
+
+import pygame
+
+
+class AlienInvasion:
+    """Класс для управления ресурсами и поведением игры"""
+
+    def __init__(self):
+        """инициализирует игру и создает игровые ресурсы"""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode(1200, 800)
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        """Запуск основного цикла игры"""
+        while True:
+            # отслеживание событий клавиатуры и мыши
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # отображение последнего прорисованного экрана
+            pygame.display.flip()
+
+
+if __name__ == '__main__':
+    # создание экземпляра и запуск игры
+    ai = AlienInvasion()
+    ai.run_game()
+
+
+
